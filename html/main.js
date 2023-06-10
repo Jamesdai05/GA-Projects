46b46ec1076d271ff23177b17a6f3774ddf92e8c
jamesdai05 <dai_guilong@hotmail.com>
2023-06-10 18:23:36 +0800

    add the js file to html folder

---
+++ html/main.js
@@ -0,0 +1,8 @@
+const para = document.querySelectorAll(".toggle");
+console.log(para);
+
+/* for (let i = 0; i < para.length; i++) {
+  console.log(para[i]);
+} */
+
+para.forEach((ele) => console.log(ele));
